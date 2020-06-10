a2c2781161f3cffb02bffec8f3cd9db3af805711
vbudithi <vbudithi@deakin.edu.au>
2020-06-11 05:00:14 +0930

    shopping-logo / product images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './shop-logo.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        Y
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vivek Budithi
         </a>
       </header>
     </div>
