c1e940d7c44c338c8c7c581fb0a14e51dcf8f3cc
vbudithi <vbudithi@deakin.edu.au>
2020-06-12 14:00:14 +0930

    route added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
+import{Switch,Route} from 'react-router-dom';
 import logo from './shop-logo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Y
-        <a
-          className="App-link"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vivek Budithi
-        </a>
-      </header>
-    </div>
-  );
-}
-
+       <React.Fragment>
+           <Navbar />
+            <Switch>   
+            <Route exact path="/" component = {ProductList} />            
+             <Route path="/details" component = {Details} />    
+             <Route path ="/cart" component ={Cart} />
+             <Route component ={Default} />
+            </Switch>
+       </React.Fragment>
+    );
+   }
+ }
 export default App;
